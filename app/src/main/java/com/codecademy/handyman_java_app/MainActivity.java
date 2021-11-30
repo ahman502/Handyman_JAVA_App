fa26eebf5504f6db6efcbff7ec56c62cc2603556
Natalia Ahmad <nataliaahmadhayat@gmail.com>
2021-11-29 19:25:18 -0500

    Created a sign-up button for registration. Added functionality to sign in and sign up buttons. Added functionality to check for validation of multiple data fields.

--- app/src/main/java/com/codecademy/handyman_java_app/MainActivity.java
+++ app/src/main/java/com/codecademy/handyman_java_app/MainActivity.java
@@ -2,13 +2,71 @@ package com.codecademy.handyman_java_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email_input, password_input;
+    Button submit_btn, sign_up_btn, forgot_password;
+    boolean isEmailValid, isPasswordValid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
+        submit_btn = findViewById(R.id.submit_btn);
+        sign_up_btn = findViewById(R.id.sign_up_btn);
+        forgot_password = findViewById(R.id.forgot_password);
+
+        submit_btn.setOnClickListener(v -> SetValidation());
+
+        sign_up_btn.setOnClickListener(v -> {
+            // redirect to Register Activity
+            openNewActivity();
+        });
     }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
+    public void SetValidation() {
+        // Check for a valid email address.
+        if (email_input.getText().toString().isEmpty()) {
+            email_input.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email_input.getText().toString()).matches()) {
+            email_input.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else  {
+            isEmailValid = true;
+        }
+
+        // Check for a valid password.
+        if (password_input.getText().toString().isEmpty()) {
+            password_input.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password_input.getText().length() < 6) {
+            password_input.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else  {
+            isPasswordValid = true;
+        }
+
+        if (isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/codecademy/handyman_java_app/Register.java
+++ app/src/main/java/com/codecademy/handyman_java_app/Register.java
@@ -2,13 +2,96 @@ package com.codecademy.handyman_java_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    EditText firstName, lastName, email_address, password, confirm_password;
+    boolean isFirstValid, isLastValid, isEmailValid, isPasswordValid;
+    Button submit_btn, sign_up_btn;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        email_address = findViewById(R.id.email_address);
+        password = findViewById(R.id.password);
+        confirm_password = findViewById(R.id.confirm_password);
+        sign_up_btn = findViewById(R.id.sign_up_btn);
+        submit_btn = findViewById(R.id.submit_btn);
+
+        sign_up_btn.setOnClickListener(v -> SetValidation());
+
+        submit_btn.setOnClickListener(v -> {
+            // redirect to Login Activity
+            openNewActivity();
+        });
+
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.categories, R.layout.spinner_item);
+        spinner.setAdapter(adapter);
+
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void SetValidation() {
+        // Check for a valid first name.
+        if (firstName.getText().toString().isEmpty()) {
+            firstName.setError(getResources().getString(R.string.name_error));
+            isFirstValid = false;
+        } else  {
+            isFirstValid = true;
+        }
+
+        // Check for a valid last name.
+        if (lastName.getText().toString().isEmpty()) {
+            lastName.setError(getResources().getString(R.string.name_error));
+            isLastValid = false;
+        } else  {
+            isLastValid = true;
+        }
+
+        // Check for a valid email address.
+        if (email_address.getText().toString().isEmpty()) {
+            email_address.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email_address.getText().toString()).matches()) {
+            email_address.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else  {
+            isEmailValid = true;
+        }
+
+        // Check for a valid password.
+        if (password.getText().toString().isEmpty()) {
+            password.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            password.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else  {
+            isPasswordValid = true;
+        }
+
+        if (isFirstValid && isLastValid && isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
