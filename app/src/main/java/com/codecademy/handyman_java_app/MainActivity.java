b158820e79608d7e54a5d037004bcbc7ebe40e67
Natalia Ahmad <nataliaahmadhayat@gmail.com>
2021-11-30 17:34:33 -0500

    Fixed errors on registration page. Added functionality to check for valid password. Added comments throughout the program.

--- app/src/main/java/com/codecademy/handyman_java_app/MainActivity.java
+++ app/src/main/java/com/codecademy/handyman_java_app/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
 
+    //declaring the variables that will be used
     EditText email_input, password_input;
     Button submit_btn, sign_up_btn, forgot_password;
     boolean isEmailValid, isPasswordValid;
@@ -20,53 +25,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing all the variables using the ids used in the layout files
         email_input = findViewById(R.id.email_input);
         password_input = findViewById(R.id.password_input);
         submit_btn = findViewById(R.id.submit_btn);
         sign_up_btn = findViewById(R.id.sign_up_btn);
         forgot_password = findViewById(R.id.forgot_password);
 
-        submit_btn.setOnClickListener(v -> SetValidation());
-
-        sign_up_btn.setOnClickListener(v -> {
-            // redirect to Register Activity
-            openNewActivity();
-        });
+        //setting on click listeners on the buttons (sign in and sign up) of registration screen
+        submit_btn.setOnClickListener(v -> SetValidation());                      //sign in button to sign into the application
+        sign_up_btn.setOnClickListener(v -> openNewActivity());                   //sign up button to take to registration form
     }
 
+    //function to open and display a new activity
     public void openNewActivity(){
+        //when this function is called, we will be navigated to the register.xml file (Register.java class)
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
     }
 
+    // function to check for validation of multiple data fields
     public void SetValidation() {
+
         // Check for a valid email address.
         if (email_input.getText().toString().isEmpty()) {
+            //if the email field is empty we get an error that the field is empty
             email_input.setError(getResources().getString(R.string.email_error));
             isEmailValid = false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email_input.getText().toString()).matches()) {
+        }
+        //if the email address is missing a pattern like @ symbol or .com extension then we get an error
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email_input.getText().toString()).matches()) {
             email_input.setError(getResources().getString(R.string.error_invalid_email));
             isEmailValid = false;
-        } else  {
+        }
+        // otherwise the email address is valid and proceed with the registration form
+        else  {
             isEmailValid = true;
         }
 
         // Check for a valid password.
         if (password_input.getText().toString().isEmpty()) {
+            // if password field is empty, we get an error that the field is empty
             password_input.setError(getResources().getString(R.string.password_error));
             isPasswordValid = false;
-        } else if (password_input.getText().length() < 6) {
+        }
+        // if the length of the password is less than 8 characters, we get an error
+        else if (password_input.getText().length() < 8) {
             password_input.setError(getResources().getString(R.string.error_invalid_password));
             isPasswordValid = false;
-        } else  {
+        }
+        // if the isValidPassword function returns false then the password is missing an uppercase, lowercase, special character or a digit
+        else if (!isValidPassword(password_input.getText().toString())){
+            password_input.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        }
+        // otherwise the password is valid
+        else {
             isPasswordValid = true;
         }
 
+        // if both email and password are valid then we can proceed with the login
         if (isEmailValid && isPasswordValid) {
-            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+            //display a login successful toast message
+            Toast.makeText(getApplicationContext(), "Sign in successful", Toast.LENGTH_SHORT).show();
+            //hide the keyboard after we are done typing on the emulator's keyboard
+            password_input.onEditorAction(EditorInfo.IME_ACTION_DONE);
         }
-
     }
 
+    //function to check if the entered password is valid using Java Regex. Takes in a password as the argument
+    public boolean isValidPassword(String password) {
 
+        //using Java's pattern and matcher classes with Regex
+        Pattern pattern;
+        Matcher matcher;
+
+        // password criteria not format: digit + lowercase char + uppercase char + punctuation + symbol + length
+        // At least one lowercase character, one uppercase character, one digit, one special character, and length between 8 to 20
+        String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        //get the result using matches() method
+        return matcher.matches();
+
+    }
 }
+
+
--- app/src/main/java/com/codecademy/handyman_java_app/Register.java
+++ app/src/main/java/com/codecademy/handyman_java_app/Register.java
@@ -11,10 +11,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Register extends AppCompatActivity {
 
+    //declaring the variables that will be used
     EditText firstName, lastName, email_address, password, confirm_password;
-    boolean isFirstValid, isLastValid, isEmailValid, isPasswordValid;
+    boolean isFirstValid, isLastValid, isEmailValid, isPasswordValid, isConfirmPasswordValid;
     Button submit_btn, sign_up_btn;
     Spinner spinner;
 
@@ -23,6 +27,7 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //initializing all the variables using the ids used in the layout files
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         email_address = findViewById(R.id.email_address);
@@ -31,67 +36,127 @@ public class Register extends AppCompatActivity {
         sign_up_btn = findViewById(R.id.sign_up_btn);
         submit_btn = findViewById(R.id.submit_btn);
 
-        sign_up_btn.setOnClickListener(v -> SetValidation());
+        //setting on click listeners on the buttons (sign in and sign up) of registration screen
+        sign_up_btn.setOnClickListener(v -> SetValidation());               //sign up button to take to registration form
+        submit_btn.setOnClickListener(v -> openNewActivity());              //sign in button to sign into the application
 
-        submit_btn.setOnClickListener(v -> {
-            // redirect to Login Activity
-            openNewActivity();
-        });
-
+        //declaring a spinner
         spinner = findViewById(R.id.spinner);
-
+        /*adapter for spinner that uses items in the strings.xml file to fill the spinner (drop down menu on registration page)
+        and uses spinner_item.xml resource file for the layout (styling) of the spinner */
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.categories, R.layout.spinner_item);
         spinner.setAdapter(adapter);
 
     }
 
+    //function to open and display a new activity
     public void openNewActivity(){
+        //when this function is called, we will be navigated to the activity_main.xml file (MainActivity.java class)
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    // function to check for validation of multiple data fields
     public void SetValidation() {
+
         // Check for a valid first name.
         if (firstName.getText().toString().isEmpty()) {
+            //if the first name field is empty, we get an error that the field is empty
             firstName.setError(getResources().getString(R.string.name_error));
             isFirstValid = false;
-        } else  {
+        }
+        //if it's filled with some information, we can proceed with the rest of the registration
+        else {
             isFirstValid = true;
         }
 
         // Check for a valid last name.
         if (lastName.getText().toString().isEmpty()) {
+            //if the last name field is empty, we get an error that the field is empty
             lastName.setError(getResources().getString(R.string.name_error));
             isLastValid = false;
-        } else  {
+        }
+        //if it's filled with some information, we can proceed with the rest of the registration
+        else {
             isLastValid = true;
         }
 
         // Check for a valid email address.
         if (email_address.getText().toString().isEmpty()) {
+            //if the email field is empty we get an error that the field is empty
             email_address.setError(getResources().getString(R.string.email_error));
             isEmailValid = false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email_address.getText().toString()).matches()) {
+        }
+        //if the email address is missing a pattern like @ symbol or .com extension then we get an error
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email_address.getText().toString()).matches()) {
             email_address.setError(getResources().getString(R.string.error_invalid_email));
             isEmailValid = false;
-        } else  {
+        }
+        // otherwise the email address is valid and proceed with the registration form
+        else  {
             isEmailValid = true;
         }
 
         // Check for a valid password.
         if (password.getText().toString().isEmpty()) {
+            // if password field is empty, we get an error that the field is empty
             password.setError(getResources().getString(R.string.password_error));
             isPasswordValid = false;
-        } else if (password.getText().length() < 6) {
+        }
+        // if the length of the password is less than 8 characters, we get an error
+        else if (password.getText().length() < 8) {
             password.setError(getResources().getString(R.string.error_invalid_password));
             isPasswordValid = false;
-        } else  {
+        }
+        // if the isValidPassword function returns false then the password is missing an uppercase, lowercase, special character or a digit
+        else if (!isValidPassword(password.getText().toString())){
+            password.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        }
+        //otherwise we proceed with the registration form
+        else  {
             isPasswordValid = true;
         }
 
-        if (isFirstValid && isLastValid && isEmailValid && isPasswordValid) {
-            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        // Check for a valid confirm password.
+        if (confirm_password.getText().toString().isEmpty()) {
+            // if confirm password field is empty, we get an error that the field is empty
+            confirm_password.setError(getResources().getString(R.string.confirm_password));
+            isConfirmPasswordValid = false;
         }
+        // if confirm password is not the same as the password, we get a password mismatch message
+        else if (!confirm_password.getText().toString().equals(password.getText().toString())) {
+            confirm_password.setError(getResources().getString(R.string.passwords_dont_match));
+            isConfirmPasswordValid = false;
+        }
+        // otherwise both passwords match and the we can proceed
+        else {
+            isConfirmPasswordValid = true;
+        }
+
+        // if all the fields are true then we can go ahead and register
+        if (isFirstValid && isLastValid && isEmailValid && isPasswordValid && isConfirmPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Sign up successful", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    //function to check if the entered password is valid using Java Regex. Takes in a password as the argument
+    public boolean isValidPassword(String password) {
+
+        //using Java's pattern and matcher classes with Regex
+        Pattern pattern;
+        Matcher matcher;
+
+        // password criteria not format: digit + lowercase char + uppercase char + punctuation + symbol + length
+        // At least one lowercase character, one uppercase character, one digit, one special character, and length between 8 to 20
+        String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$";
+
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        //get the result using matches() method
+        return matcher.matches();
 
     }
 }
